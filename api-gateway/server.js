3f6ef9ba491ded3f65a65762699971190d33ddff
GlobalCodes <glorychukwuzurum@gmail.com>
2026-06-17 09:48:26 +0000

    cors update

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -16,11 +16,15 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(helmet())
 app.use(cors({
-  origin: 'https://freeblogweb.vercel.app',
-  credentials: true
-}))
+  origin: ['https://freeblogweb.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+app.options('*', cors());
+
+app.use(helmet())
+
 app.use(cookieParser())
 
 const redisClient = new Redis(process.env.REDIS_URL);
--- post-service/server.js
+++ post-service/server.js
@@ -17,11 +17,13 @@ const app = express();
 const PORT = process.env.PORT || 6000
 
 app.use(express.json())
-app.use(helmet())
 app.use(cors({
-  origin: 'https://freeblogweb.vercel.app',
-  credentials: true
-}))
+  origin: ['https://freeblogweb.vercel.app', 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+app.options('*', cors());
+app.use(helmet())
 
 mongoose.connect(process.env.MONGO_DB_URL).then(() => {
   return postLogger.info('Connected To MongoDB Successfully')
